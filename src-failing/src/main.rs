e840084a11bf8fd1b13c99292b84fd68b4092b2b
Antoine Lavandier <antoine.lavandier@inria.fr>
2025-07-22 17:18:56 +0200

    Remove more useless code
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- src-failing/src/main.rs
+++ src-failing/src/main.rs
@@ -1,5 +1,4 @@
 use wasmtime::{component::Component, *};
-use wasmtime::component::bindgen;
 
 fn main () {
     if let Err(e) = run_wasmtime() {
@@ -13,9 +12,9 @@ fn run_wasmtime() -> wasmtime::Result<()> {
 
     config.target("pulley32")?;
     let engine = Engine::new(&config)?;
-    let component_bytes  = include_bytes!("path/to/add_wasm32-wasip1.cwasm");
+    let component_bytes  = include_bytes!("../../component-model-precompile/add_wasm32-wasip1.cwasm");
 
-    let component = match unsafe { Component::deserialize_raw(&engine, component_bytes.as_slice().into()) } {
+    let _component = match unsafe { Component::deserialize_raw(&engine, component_bytes.as_slice().into()) } {
         Ok(comp) => comp,
         Err(error) => {
             println!("failed to deserialize pre-compiled module: {:?}", defmt::Debug2Format(&error));
@@ -23,8 +22,5 @@ fn run_wasmtime() -> wasmtime::Result<()> {
         }
     };
     println!("Deserialized Component");
+    Ok(())
 }
-
-
-
-
